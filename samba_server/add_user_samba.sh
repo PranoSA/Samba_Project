b5dd8c924cf4ddb43a69755edfa2c0b760497471
PranoSA <pcadler@gmail.com>
2023-09-08 18:21:36 -0700

    Fixed Shell Scripts For Samba Create and User Add

--- samba_server/add_user_samba.sh
+++ samba_server/add_user_samba.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-USER = $1
-PASSWORD = $2
+# sudo sh add_user_samba.sh "wadler-gmail.com" "password" "boba"
 
-SHARE_ID = $3
+SAMBA_USER=$1
+SAMBA_PASSWORD=$2
+
+SHARE_ID=$3
+
+SPACE_ID=$4
 
 
-useradd -M -G $SPACE_ID $USER:$SHARE_ID
+useradd -M -G $SPACE_ID $SAMBA_USER-$SHARE_ID
 
-(echo $PASSWORD; echo $PASSWORD) | smbpasswd -a $USER:$SHARE_ID
+(echo $SAMBA_PASSWORD; echo $SAMBA_PASSWORD) | smbpasswd -a $SAMBA_USER-$SHARE_ID
 
 
 #awk '{sub(/valid users = */,"valid users = $USER ")}1' samba_shell_test.txt 
 #awk '{sub(/valid users = */,"valid users = poop ")}1' samba_shell_test.txt 
 
-awk -v user=$USER '{sub(/valid users = */,"valid users = "user" ")}1' /etc/samba/smb.conf.d/$SHARE_ID
+awk -v user=$SAMBA_USER-$SHARE_ID '{sub(/valid users = */,"valid users = "user" ")}1' /etc/samba/smb.conf.d/$SHARE_ID.conf>here.txt # >/etc/samba/smb.conf.d/$SHARE_ID.conf
+cat here.txt > /etc/samba/smb.conf.d/$SHARE_ID.conf
 
 #awk -v user="poop" '{sub(/valid users = */,"valid users = "user" ")}1' samba_shell_test.txt 
 
--- samba_server/create_samba.sh
+++ samba_server/create_samba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-USER=$1
-PASSWORD=$1
+
+#Test sudo sh create_samba.sh /mount/samba_server/1 "boba" "pcadler-gmail.com" "password" 1 
 
 MOUNT_PATH=$1
 SHARE_ID=$2
@@ -9,97 +9,74 @@ OWNER=$3
 OWNER_PASSWORD=$4
 SPACE_ID=$5
 
-if [ -z "$1"]; then
+if [ -z "$1" ]; then
     echo "No Mount Path Given"
-    exit 1
-fi
+    #exit 1
+fi 
 
-addgroup $SPACE_ID
+#addgroup samba-$SPACE_ID
 
-status $?
+status=$?
 
-if [ $status -ne 0]; then
+if [ $status -ne 0 ]; then
     echo "Failed To Add Group"
-    exit 1
+   # exit 1
 fi
 
-useradd -M -G $SPACE_ID $USER:$SHARE_ID
+useradd -M -G $SPACE_ID $OWNER-$SHARE_ID
 
-status $?
+status=$?
 
-if [ $status -ne 0]; then
+if [ $status -ne 0 ]; then
     echo "Failed To Add User"
-    exit 1
+    #exit 1
 fi
 
 cat >/etc/samba/smb.conf.d/$SHARE_ID.conf <<EOF
 [$SHARE_ID]
     path = $MOUNT_PATH/$SHARE_ID
-    browseable = no
+    browseable = yes
     read only = no
     force create mode = 0777
     force directory mode = 0777
     create mask = 0777
     directory mask = 0777
-    valid users = $OWNER
+    valid users = $OWNER-$SHARE_ID
 EOF
 
-status $?
+status=$?
 
-if [ $status -ne 0]; then
+if [ $status -ne 0 ]; then
     echo "Failed To Write to Samba Config "
-    exit 1
+    #exit 1
 fi
 
 (
     echo $OWNER_PASSWORD
-    echo $PASSWORD
-) | smbpasswd -a $OWNER:$OWNER_SHARE_ID
+    echo $OWNER_PASSWORD
+) | smbpasswd -a $OWNER-$SHARE_ID
 
-status $?
+status=$?
 
-if [ $status -ne 0]; then
+if [ $status -ne 0 ]; then
     echo "Failed To Set Samba password"
-    exit 1
+    #exit 1
 fi
 
 ls /etc/samba/smb.conf.d/* | sed -e 's/^/include = /' >/etc/samba/includes.conf
 
-status $?
+status=$?
 
-if [ $status -ne 0]; then
+if [ $status -ne 0 ]; then
     echo "Failed To Add TO Include File"
-    exit 1
+    #exit 1
 fi
 
 smbcontrol all reload-config
 
-status $?
+status=$?
 
-if [ $status -ne 0]; then
+if [ $status -ne 0 ]; then
     echo "Failed To Reload Samba"
-    exit 1
+    #exit 1
 fi
-
-bob = ''' useradd -M -d /backups/$USER -s /usr/sbin/nologin -G sambashare $USER
-mkdir /backups/$USER
-chown $USER:sambashare /backups/$USER
-chmod 2770 /backups/$USER
-smbpasswd -a $USER
-smbpasswd -e $USER
-
-cat  /etc/samba/smb.conf.d/$USER.conf
-[$USER]
-    path = /$mount_pa/$USER
-    browseable = no
-    read only = no
-    force create mode = 0660
-    force directory mode = 2770
-    valid users = $USER
-EOF
-
-ls /etc/samba/smb.conf.d/* | sed -e 's/^/include = /' > /etc/samba/includes.conf
-
-smbcontrol all reload-config
-
-'''
